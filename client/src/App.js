d024f171c470ca9fdc7fcc0c5f3a8014c185d1b2
pillious <andrewzhlee@gmail.com>
2022-04-08 16:32:59 -0400

    initial

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+// import classes from './App.module.css';
+
+function App() {
+  return (
+    <div>
+      Testing.
+    </div>
+  );
+}
+
+export default App;
---
+++ parser/src/parser/Parser.java
@@ -0,0 +1,9 @@
+package parser;
+
+public class Parser {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+
+}
